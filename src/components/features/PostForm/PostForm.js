123e1805445ba92f05bf7eeb1e7896c137a9dd4c
Dawid <daw.parc@gmail.com>
2023-02-20 20:26:05 +0100

    Add form validate

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -4,6 +4,7 @@ import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 import DatePicker from "react-datepicker";
 import 'react-datepicker/dist/react-datepicker.css';
+import { useForm } from "react-hook-form";
 
 const PostForm = ({ action, actionText, ...props }) => {
 
@@ -12,37 +13,48 @@ const PostForm = ({ action, actionText, ...props }) => {
   const [publishedDate, setPublishedDate] = useState(props.publishedDate || '');
   const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
   const [content, setContent] = useState(props.content || '');
+  const [contentError, setContentError] = useState(false);
+  const [dateError, setDateError] = useState(false);
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    action({ 
-      title, 
-      author, 
-      publishedDate, 
-      shortDescription, 
-      content 
-    });
+  const { register, handleSubmit: validate, formState: { errors } } = useForm();
+
+  const handleSubmit = () => {
+    setContentError(!content)
+    setDateError(!publishedDate)
+    if(content && publishedDate){
+      action({ 
+        title, 
+        author, 
+        publishedDate, 
+        shortDescription, 
+        content 
+      });
+    };
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={validate(handleSubmit)}>
       <Form.Group className="mb-3 col-lg-6" controlId="title">
         <Form.Label>Title</Form.Label>
         <Form.Control 
+          {...register("title", { required: true, minLength: 3 })}
           value={title}
           type="text" 
           placeholder="Enter title" 
           onChange={e => setTitle(e.target.value)} 
         />
+        {errors.title && <small className="d-block form-text text-danger mt-2">Title is too short (min is 3)</small>}      
       </Form.Group>
       <Form.Group className="mb-3 col-lg-6" controlId="postAuthor">
         <Form.Label>Author</Form.Label>
         <Form.Control 
+          {...register("author", { required: true, minLength: 3 })}
           value={author}
           type="text" 
           placeholder="Enter author" 
           onChange={e => setAuthor(e.target.value)} 
         />
+        {errors.author && <small className="d-block form-text text-danger mt-2">Author is too short (min is 3)</small>}      
       </Form.Group>
       <Form.Group className="mb-3 col-lg-6" controlId="postPublishedDate">
         <Form.Label>Published</Form.Label>
@@ -50,17 +62,19 @@ const PostForm = ({ action, actionText, ...props }) => {
           selected={publishedDate}
           placeholderText="  Enter date"
           onChange={(date) => setPublishedDate(date)} 
-          required
         />
+        {dateError && <small className="d-block form-text text-danger mt-2">This field is required</small>}
       </Form.Group>
       <Form.Group className="mb-3" controlId="postShortDescription">
         <Form.Label>Short description</Form.Label>
         <Form.Control 
+          {...register("shortDescription", { required: true, minLength: 20 })}
           value={shortDescription}
           as="textarea" 
           placeholder="Leave a comment here" 
           onChange={e => setShortDescription(e.target.value)} 
         />
+        {errors.shortDescription && <small className="d-block form-text text-danger mt-2">Description is too short (min is 20)</small>}
       </Form.Group>
       <Form.Group className="mb-3" controlId="postContent">
         <Form.Label>Main content</Form.Label>
@@ -70,12 +84,12 @@ const PostForm = ({ action, actionText, ...props }) => {
           onChange={setContent}
           theme="snow"
         />
+        {contentError && <small className="d-block form-text text-danger mt-2">Content can't be empty</small>}
       </Form.Group>
       <Button variant="primary" type="submit">
         {actionText}
       </Button>
     </Form>
-
   );
 };
 
